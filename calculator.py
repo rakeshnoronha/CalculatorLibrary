f0c8f41b2956b7341fccb3e7105a227589500249
rakeshnoronha <rakesh.noronha@gmail.com>
2023-04-02 22:23:01 +1000

    adding cal functions

---
+++ calculator.py
@@ -0,0 +1,9 @@
+"""
+Calculator Library contains basic math operations
+"""
+
+def add(first_num, second_num):
+    return first_num + second_num
+
+def subtract(first_num, second_num):
+    return first_num - second_num
