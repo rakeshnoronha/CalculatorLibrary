db8d7f695db6072b15ecb82c3722d65871653c3f
rakeshnoronha <rakesh.noronha@gmail.com>
2023-04-02 22:33:50 +1000

    add unit tests

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@
 Calculator Library contains basic math operations
 """
 
+
 def add(first_num, second_num):
     return first_num + second_num
+
 
 def subtract(first_num, second_num):
     return first_num - second_num
